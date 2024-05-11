4def461619b6f9cf832189a60718b5557f046b3b
Timothy Arnold <timothy.cy.arnold@gmail.com>
2024-05-11 02:22:55 +0100

    Re-enables batch normalization, stratifies train/val split

--- train_model.py
+++ train_model.py
@@ -1,29 +1,43 @@
+import time
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.nn.init as init
 from torch.utils.data import DataLoader
-from torch.utils.data import random_split 
+from torch.utils.data import Subset
 from torchvision import datasets, transforms
-import time
-import numpy as np
+from sklearn.model_selection import train_test_split
 
-np.random.seed(1)
-torch.manual_seed(1)
+seed = 123
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
 
 device = "cuda"
 
 transform=transforms.ToTensor() 
 
-# Split MNIST dataset into train and test
-train_set = datasets.MNIST(root='data', train=True, download=True, transform=transform)
-train_set, validation_set = random_split(train_set, [int(len(train_set) * 5/6), len(train_set) - int(len(train_set) * 5/6)])
+# Split MNIST dataset into train and test, stratifying on label
+train_set_full = datasets.MNIST(root='data', train=True, download=True, transform=transform)
+labels = [datapoint[1] for datapoint in train_set_full]
+train_indices, val_indices = train_test_split(
+    list(range(len(train_set_full))), 
+    test_size=1/6, 
+    stratify=labels, 
+    random_state=42
+)
+# Create Subset objects for train and test sets
+train_set = Subset(train_set_full, train_indices)
+validation_set = Subset(train_set_full, val_indices)
 test_set = datasets.MNIST(root='data', train=False, download=True, transform=transform)
 
 train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
 validation_loader = DataLoader(validation_set, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_set, batch_size=64)
+
 
 class MNISTCNN(nn.Module):
     def __init__(self):
@@ -39,14 +53,14 @@ class MNISTCNN(nn.Module):
         init.kaiming_uniform_(self.fc2.weight)
 
         # Batch Normalization
-        # self.bn1 = nn.BatchNorm1d(100)
+        self.bn1 = nn.BatchNorm1d(100)
 
     def forward(self, x):
         x = F.elu(F.max_pool2d(self.conv1(x), kernel_size=2, stride=2))
         x = F.elu(F.max_pool2d(self.conv2_drop(self.conv2(x)), kernel_size=2, stride=2))
         x = x.view(-1, 900)
         x = F.elu(self.fc1(x))
-        # x = self.bn1(x)
+        x = self.bn1(x)
         x = F.dropout(x, training=self.training, p=0.4)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
@@ -72,13 +86,10 @@ def train(model, train_loader, max_epochs, early_stopping_steps):
     optimizer = optim.Adam(model.parameters())
 
     validation_decrease_counter = 0
-
     highest_validation_accuracy = 0
-
     stopped_early = False
 
     for epoch in range(max_epochs):
-
         for batch in train_loader:
             images, labels = batch
             images, labels = images.to(device), labels.to(device)
@@ -90,7 +101,7 @@ def train(model, train_loader, max_epochs, early_stopping_steps):
 
         train_accuracy = test_accuracy(model, train_loader)
         validation_accuracy = test_accuracy(model, validation_loader)
-        print(f"Epoch {epoch + 1}  -  Train accuracy: {format(train_accuracy, '.2f')}%  -  Validation accuracy: {format(validation_accuracy, '.2f')}%")
+        print(f"Epoch {epoch + 1} - Train accuracy: {format(train_accuracy, '.2f')}% - Validation accuracy: {format(validation_accuracy, '.2f')}%")
 
         if validation_accuracy > highest_validation_accuracy:
             highest_validation_accuracy = validation_accuracy
@@ -106,8 +117,8 @@ def train(model, train_loader, max_epochs, early_stopping_steps):
     if not stopped_early:
         print("Maximum number of epochs reached")
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     max_epochs = 50
     early_stopping_steps = 5
 
@@ -123,7 +134,6 @@ if __name__ == "__main__":
     print(f"Time taken: {np.round(time_taken, 1)}s")
 
     test_accuracy = test_accuracy(model, test_loader)
-
     print("-" * 60)
     print('Test accuracy: ', format(test_accuracy, '.2f'), "%")
     print("-" * 60)
